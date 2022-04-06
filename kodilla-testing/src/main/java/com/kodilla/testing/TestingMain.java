503a6b9ea9a3c2d7d7fd72a95fbcb179cd4f3a4e
pstruzyn <pawel.struzynski@pentacomp.pl>
2022-04-06 08:14:22 +0200

    6.2 Added first test Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        Calculator calculator = new Calculator();
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int resultAdd = calculator.add(5,5);
+
+        if (resultAdd == 10) {
+            System.out.println("test add OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int resultSubtract = calculator.subtract(10,8);
+
+        if (resultSubtract == 2) {
+            System.out.println("test subtract OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
