319d5edd15ce4a90a27cee681489a1bc7f7e367c
pstruzyn <pawel.struzynski@pentacomp.pl>
2022-04-14 13:28:46 +0200

    7.3 Zadanie: funkcyjny spacer po liście użytkowników forum

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -10,7 +10,7 @@ public class Forum {
 
     public Forum() {
         thisList.add(new ForumUser(1,"John Kowalski",'M', LocalDate.of(1990,10,5),100));
-        thisList.add(new ForumUser(2,"Wayne Nowak",'M',LocalDate.of(2005,1,10),50));
+        thisList.add(new ForumUser(2,"Wayne Nowacki",'M',LocalDate.of(2005,1,10),50));
         thisList.add(new ForumUser(3,"Marta Malinowska",'F',LocalDate.of(1990,9,16),0));
     }
 
