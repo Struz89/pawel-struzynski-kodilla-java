7816a7df5d86d95605604576c61f692a13d1116e
pstruzyn <pawel.struzynski@pentacomp.pl>
2022-04-16 00:17:19 +0200

    6.7. Zadanie dodatkowe

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsClass.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsClass.java
@@ -2,12 +2,40 @@ package com.kodilla.testing.forum.statistics;
 
 public class StatisticsClass {
 
-    public StatisticsClass (Statistics statistics) {
+    public double users = 0;
+    public double posts = 0;
+    public double comments = 0;
 
-    }
+    public double averagePostsPerUsers = 0;
+    public double averageCommentsPerUsers = 0;
+    public double averageCommentsPerPosts = 0;
 
-    public void calculateAdvStatistics(Statistics statistics) {
-
+    public double calculateAdvStatistics(Statistics statistics) {
+        if (statistics.usersNames().size() > 0 && statistics.postsCount() == 0 && statistics.commentsCount() == 0) {
+            users = statistics.usersNames().size();
+            return users;
+        } else if (statistics.postsCount() > 0 && statistics.usersNames().size() == 0 && statistics.commentsCount() == 0) {
+            posts = statistics.postsCount();
+            return posts;
+        } else if (statistics.commentsCount() > 0 && statistics.usersNames().size() == 0 && statistics.postsCount() == 0) {
+            comments = statistics.commentsCount();
+            return comments;
+        } else if (statistics.usersNames().size() > 0 && statistics.postsCount() > 0 && statistics.commentsCount() == 0) {
+            averagePostsPerUsers = statistics.postsCount()/statistics.usersNames().size();
+            return averagePostsPerUsers;
+        } else if (statistics.usersNames().size() > 0 && statistics.postsCount() == 0 && statistics.commentsCount() > 0) {
+            averageCommentsPerUsers = statistics.commentsCount()/statistics.usersNames().size();
+            return averageCommentsPerUsers;
+        } else if (statistics.usersNames().size() == 0 && statistics.postsCount() > 0 && statistics.commentsCount() > 0) {
+            averageCommentsPerPosts = statistics.commentsCount()/statistics.postsCount();
+            return averageCommentsPerPosts;
+        } else if (statistics.usersNames().size() > 0 && statistics.postsCount() > 0 && statistics.commentsCount() > 0) {
+            users = statistics.usersNames().size();
+            posts = statistics.postsCount();
+            comments = statistics.commentsCount();
+            return users + posts + comments;
+        }
+        return 0;
     }
 
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsClassTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsClassTestSuite.java
@@ -28,58 +28,150 @@ public class StatisticsClassTestSuite {
     @Test
     void testUsersEqualTo0() {
         // Given
-        StatisticsClass statisticsClass = new StatisticsClass(statisticsMock);
+        StatisticsClass statisticsClass = new StatisticsClass();
         List<String> userName = new ArrayList<>();
         when(statisticsMock.usersNames()).thenReturn(userName);
 
         // When
-        statisticsClass.calculateAdvStatistics(statisticsMock);
+        double users = statisticsClass.calculateAdvStatistics(statisticsMock);
 
         // Then
-        //Assertions.assertEquals(0,users);
+        Assertions.assertEquals(0,users);
 
     }
 
     @Test
     void testUsersEqualTo100() {
         // Given
-        StatisticsClass statisticsClass = new StatisticsClass(statisticsMock);
+        StatisticsClass statisticsClass = new StatisticsClass();
         List<String> userName = generateUsers("Pablo", 100);
         when(statisticsMock.usersNames()).thenReturn(userName);
 
         // When
-        statisticsClass.calculateAdvStatistics(statisticsMock);
+        double users = statisticsClass.calculateAdvStatistics(statisticsMock);
 
         // Then
-        //Assertions.assertEquals(100,users);
+        Assertions.assertEquals(100,users);
 
     }
 
     @Test
     void testPostsEqualTo0() {
         // Given
-        StatisticsClass statisticsClass = new StatisticsClass(statisticsMock);
+        StatisticsClass statisticsClass = new StatisticsClass();
         when(statisticsMock.postsCount()).thenReturn(0);
 
         // When
-        statisticsClass.calculateAdvStatistics(statisticsMock);
+        double posts = statisticsClass.calculateAdvStatistics(statisticsMock);
 
         // Then
-        //Assertions.assertEquals(0,users);
+        Assertions.assertEquals(0,posts);
 
     }
 
     @Test
-    void testPostsEqualTo100() {
+    void testPostsEqualTo1000() {
         // Given
-        StatisticsClass statisticsClass = new StatisticsClass(statisticsMock);
-        when(statisticsMock.postsCount()).thenReturn(100);
+        StatisticsClass statisticsClass = new StatisticsClass();
+        when(statisticsMock.postsCount()).thenReturn(1000);
 
         // When
-        statisticsClass.calculateAdvStatistics(statisticsMock);
+        double posts = statisticsClass.calculateAdvStatistics(statisticsMock);
 
         // Then
-        //Assertions.assertEquals(0,users);
+        Assertions.assertEquals(1000,posts);
+
+    }
+
+    @Test
+    void testCommentsEqualTo0() {
+        // Given
+        StatisticsClass statisticsClass = new StatisticsClass();
+        when(statisticsMock.commentsCount()).thenReturn(0);
+
+        // When
+        double comments = statisticsClass.calculateAdvStatistics(statisticsMock);
+
+        // Then
+        Assertions.assertEquals(0,comments);
+
+    }
+
+    @Test
+    void testCommentsEqualTo100() {
+        // Given
+        StatisticsClass statisticsClass = new StatisticsClass();
+        when(statisticsMock.commentsCount()).thenReturn(100);
+
+        // When
+        double comments = statisticsClass.calculateAdvStatistics(statisticsMock);
+
+        // Then
+        Assertions.assertEquals(100,comments);
+
+    }
+
+    @Test
+    void testAveragePostsPerUsers() {
+        // Given
+        StatisticsClass statisticsClass = new StatisticsClass();
+        List<String> userName = generateUsers("Pablo", 10);
+        when(statisticsMock.usersNames()).thenReturn(userName);
+        when(statisticsMock.postsCount()).thenReturn(1000);
+
+        // When
+        double averagePostsPerUsers = statisticsClass.calculateAdvStatistics(statisticsMock);
+
+        // Then
+        Assertions.assertEquals(100,averagePostsPerUsers);
+
+    }
+
+    @Test
+    void testAverageCommentsPerUsers() {
+        // Given
+        StatisticsClass statisticsClass = new StatisticsClass();
+        List<String> userName = generateUsers("Pablo", 10);
+        when(statisticsMock.usersNames()).thenReturn(userName);
+        when(statisticsMock.commentsCount()).thenReturn(1000);
+
+        // When
+        double averageCommentsPerUsers = statisticsClass.calculateAdvStatistics(statisticsMock);
+
+        // Then
+        Assertions.assertEquals(100,averageCommentsPerUsers);
+
+    }
+
+    @Test
+    void testAverageCommentsPerPosts() {
+        // Given
+        StatisticsClass statisticsClass = new StatisticsClass();
+        when(statisticsMock.postsCount()).thenReturn(1000);
+        when(statisticsMock.commentsCount()).thenReturn(1000);
+
+        // When
+        double averageCommentsPerPosts = statisticsClass.calculateAdvStatistics(statisticsMock);
+
+        // Then
+        Assertions.assertEquals(1,averageCommentsPerPosts);
+
+    }
+
+    @Test
+    void testSumUsersCommentsAndPosts() {
+        // Given
+        StatisticsClass statisticsClass = new StatisticsClass();
+        List<String> userName = generateUsers("Pablo", 10);
+        when(statisticsMock.usersNames()).thenReturn(userName);
+        when(statisticsMock.postsCount()).thenReturn(1000);
+        when(statisticsMock.commentsCount()).thenReturn(1000);
+
+        // When
+        double all = statisticsClass.calculateAdvStatistics(statisticsMock);
+
+        // Then
+        Assertions.assertEquals(2010,all);
 
     }
 
