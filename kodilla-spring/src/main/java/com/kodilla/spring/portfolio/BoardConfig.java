0c519a369cfb10772bbda17631684cda80861546
pstruzyn <pawel.struzynski@pentacomp.pl>
2022-05-24 14:50:22 +0200

    10.3 Zadanie: tablica z listami zadań

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,7 +1,6 @@
 package com.kodilla.spring.portfolio;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -35,9 +34,7 @@ public class BoardConfig {
 
     @Bean
     @Autowired
-    public Board board(@Qualifier("toDoList") TaskList toDoList,
-                       @Qualifier("inProgressList") TaskList inProgressList,
-                       @Qualifier("doneList") TaskList doneList) {
+    public Board board(TaskList toDoList, TaskList inProgressList, TaskList doneList) {
         return new Board(toDoList, inProgressList, doneList);
     }
 }
