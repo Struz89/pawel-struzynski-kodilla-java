fb7bd62606f81459d7ad0433c733345911688aa3
pstruzyn <pawel.struzynski@pentacomp.pl>
2022-07-22 23:26:33 +0200

    20.5 Zadanie: klasyfikator statystyczny książek - poprawki

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/librarya/Book.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/librarya/Book.java
@@ -1,15 +1,13 @@
 package com.kodilla.patterns2.adapter.bookclasifier.librarya;
 
-import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
-
 public class Book {
 
-    private String author;
-    private String title;
-    private int publicationYear;
-    private String signature;
+    private final String author;
+    private final String title;
+    private final int publicationYear;
+    private final String signature;
 
-    public Book(String author, String title, int publicationYear, String signature) {
+    public Book(final String author, final String title, final int publicationYear, final String signature) {
         this.author = author;
         this.title = title;
         this.publicationYear = publicationYear;
